6504be83d322e9b869c10e70daf4f51c94d007c1
qEDEN <issamghled@gmail.com>
2023-09-09 15:23:13 +0100

    4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *Description: Prints a message with proper grammar using the printf function.
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
