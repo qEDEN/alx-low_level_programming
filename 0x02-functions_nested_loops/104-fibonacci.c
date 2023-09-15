a0b2676d1913cf7b6c84792fd8be2f9d8aa2f0ee
qEDEN <issamghled@gmail.com>
2023-09-15 03:00:30 +0100

    4

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-/** 
+/**
  * main - fibonacci <3
  *
  * Purpose - no hardcode
  *
  * Return:  (Success)
  */
- int main(void)
+int main(void)
 {
 	unsigned long int i;
 	unsigned long int bef = 1;
