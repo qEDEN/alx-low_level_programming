9a3f916f3fb5516694f20de64e6f386c3ae7b5a9
EDEN <issamghled@gmail.com>
2023-10-23 18:44:20 +0100

    4

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
@@ -46,4 +44,42 @@ size_t print_listint_safe(const listint_t *head)
 	}
 
 	return (count);
+}
+/**
+ * main - Entry point for the program
+ *
+ * Description: This function serves as the entry point for the program.
+ * It creates and prints a listint_t linked list safely.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *head2;
+	listint_t *node;
+
+	head2 = NULL;
+	add_nodeint(&head2, 0);
+	add_nodeint(&head2, 1);
+	add_nodeint(&head2, 2);
+	add_nodeint(&head2, 3);
+	add_nodeint(&head2, 4);
+	add_nodeint(&head2, 98);
+	add_nodeint(&head2, 402);
+	add_nodeint(&head2, 1024);
+	print_listint_safe(head2);
+
+	head = NULL;
+	node = add_nodeint(&head, 0);
+	add_nodeint(&head, 1);
+	add_nodeint(&head, 2);
+	add_nodeint(&head, 3);
+	add_nodeint(&head, 4);
+	node->next = add_nodeint(&head, 98);
+	add_nodeint(&head, 402);
+	add_nodeint(&head, 1024);
+	print_listint_safe(head);
+
+	return (0);
 }
